139a3e246f38827e20f4102e665bc659795bd1ed
Paul moutier <moutier.paul@gmail.com>
2019-02-20 18:53:55 +0100

    changement

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,21 @@
 import http from 'http';
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import {
-    version
-} from '../package.json';
-import WebSocketServer, {
-    Server
-} from 'uws';
+import {version} from '../package.json'
+import WebSocketServer, {Server} from 'uws';
+import AppRouter from './app-router'
+import Model from './models'
+import Database from './database'
+import path from 'path'
 
-const PORT = 3000;
+const PORT = 3001;
 const app = express();
 app.server = http.createServer(app);
+
+
+//app.use(morgan('dev'));
+
 
 app.use(cors({
     exposedHeaders: "*"
@@ -23,71 +26,43 @@ app.use(bodyParser.json({
 }));
 
 
+
 app.wss = new Server({
-    server: app.server
-});
-
-let clients = [];
-
-app.wss.on('connection', (connection) => {
-    // Connect more than on client
-    const userId = clients.length + 1;
-    connection.userId = userId;
-    const newClient = {
-        ws: connection,
-        userId: userId,
-    };
-    clients.push(newClient);
-
-    console.log('New client connected with userId', userId);
-
-    connection.on('message', (message) => {
-
-        console.log("message from: ", message);
-       
-
-    });
-
-
-    // Client his disconnected and who is 
-    connection.on('close', () => {
-        console.log('client with ID ', userId, "is disconnected " );
-
-        clients = clients.filter((client) => client.userId !== userId);
-    });
-});
-
-// we can see the json of all the clients
-app.get('/', (req, res) => {
-    res.json({
-        version: version
-    })
-});
-
-app.get('/api/all_connections', (req, res, next) => {
-    return res.json({
-        people: clients,
-    })
+	server: app.server
 });
 
 
-setInterval(() => {
-    // each 3 sec this function will be executed and know the people
-    console.log(`There ${clients.length} people in the connection.`);
-    if(clients.length > 0){
-        clients.forEach((client) => {
+// static www files use express
+const wwwPath = path.join(__dirname, 'www');
 
-            console.log("client ID", client.userId);
-            // show to the client message from the server
-            const msg = `Hey ID ${client.userId}: you got a new message from server`;
-            client.ws.send(msg);
-        });
-    }
+app.use('/', express.static(wwwPath));
 
-}, 3000)
+// Connect to Mongo Database
+
+new Database().connect().then((db) => {
+
+	console.log("Successful connected to database.")
+
+	app.db = db;
+	
+}).catch((err) => {
+
+
+	throw(err);
+});
+
+
+// End connect to Mongodb Database
+
+app.models = new Model(app);
+app.routers = new AppRouter(app);
+
+
+
+
 
 app.server.listen(process.env.PORT || PORT, () => {
-    console.log(`App is running on port ${app.server.address().port}`);
+        console.log(`App is running on port ${app.server.address().port}`);
 });
 
 export default app;
